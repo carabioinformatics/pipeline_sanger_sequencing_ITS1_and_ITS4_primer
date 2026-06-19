755fa5b436272621abf11cebb539bbab68e2bd38
Cara Louw <caralouw1@gmail.com>
2026-06-19 13:58:27 +0200

    Added threshold line to the graph. Still need to test.

--- Testing/src/TIME_REPORT_PLOTTING.py
+++ Testing/src/TIME_REPORT_PLOTTING.py
@@ -5,23 +5,25 @@ import matplotlib.pyplot as plt
 def main():
     print("CL: Start with time plotting.")
     time_report = sys.argv[1]
-    if (time_report == None):
-        print("Finishing")
-        return
 
+    # Get time input values from textfile
     time_input = pd.read_csv(time_report, sep= '\t\t')
     
+    # Plot bar graph
     plt.figure(figsize=(8,4))
     plt.bar(time_input["Sample name"], time_input["Total time"])
+    # Plot threshold line at 30 seconds
+    plt.axhline(y=30, color="red", linestyle="--", linewidth=2, label="Threshold")
 
+    # Display preferences 
     plt.xlabel("Sample name")
     plt.ylabel("Execution time (s)")
     plt.title("Pipeline execution time for each sample")
-
+    plt.legend()
     plt.tight_layout()
-    plt.show()
-
+    
     plt.savefig("./Output/INT26_TIME_REPORT_GRAPH.png", dpi=300)
+    plt.show()
 
     print("CL: Done with time plotting.")
 
